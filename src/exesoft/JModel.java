fcb78ef66f089ebcca38b7ecd3ca7271f52d3129
Michał Krakiewicz <michalkrakiewicz@hotmail.com>
2013-05-10 19:42:54 +0200

    initial commit

--- src/exesoft/JModel.java
+++ src/exesoft/JModel.java
@@ -2,7 +2,11 @@ package exesoft;
 
 public class JModel{
 
-	public String encode(String toJson){ }
-	public String decode(String fromJson){ }
+	public String encode(String toJson){ 
+		return toJson;
+	}
+	public String decode(String fromJson){
+		return fromJson; 
+	}
 
 }
