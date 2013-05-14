49efbdd7b42b47233c0bd93eac4d6ca6e804886b
Michał Krakiewicz <michalkrakiewicz@hotmail.com>
2013-05-14 22:22:04 +0200

    zmieniona nazwa klasy i dodany interfejs

--- src/exesoft/JSerializeReader.java
+++ src/exesoft/JSerializeReader.java
@@ -1,19 +1,12 @@
 package exesoft;
 
-/**
- * 
- *  JSerializeReader – Klasa odpowiedzialna za odwzorowanie obiektu
- *  na podstawie informacji uzyskanych z obiektu klasy JModel, która w procesie dekodowania
- *  (metoda decode) wytworzy obiekt klasy String o strukturze jak wy¿ej w przyk³adzie.
- *  (Nie wiem jeszcze jak to bêdzie ale na 90% bêdzie zwracany przez funkcjê statyczn¹ albo
- *  i nie? obiekt klasy Object do wyrzutowania sobie przez kogoœ kto bêdzie uzywa³ tych klas)
- *     
- *  String do przetworzenia w JModel : java.lang.Osoba{imie(java.lang.String):x;nazwisko(java.lang.String):y;}
- * 
- * @author Micha³ Krakiewicz
- *
- */
-public class JSerializeReader{
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+
+public interface JSerializeReader {
+
+	Object fromMap(Map<String, List<Map<String,Object>>> map);
 	
-	
+	Boolean readObject(InputStream input);
 }
