421f10849d39c6106038e2a3d9aa1d66b76f9ea4
Michał Krakiewicz <michalkrakiewicz@hotmail.com>
2013-06-01 20:08:17 +0200

    chwilowy problem z Eclipsem

--- src/exesoft/JSerializeReaderImpl.java
+++ src/exesoft/JSerializeReaderImpl.java
@@ -8,69 +8,89 @@ import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
 
-
 /**
  * 
- *  JSerializeReader - Klasa odpowiedzialna za odwzorowanie obiektu
- *  na podstawie informacji uzyskanych z obiektu klasy JModel, która w procesie dekodowania
- *  (metoda decode) wytworzy obiekt klasy String o strukturze jak wyżej w przykładzie.
- *  (Nie wiem jeszcze jak to będzie ale na 90% będzie zwracany przez funkcję statyczną albo
- *  i nie? obiekt klasy Object do wyrzutowania sobie przez kogoś kto będzie uzywał tych klas)
- *     
- *  String do przetworzenia w JModel : java.lang.Osoba{imie(java.lang.String):x;nazwisko(java.lang.String):y;}
+ * JSerializeReader - Klasa odpowiedzialna za odwzorowanie obiektu na podstawie
+ * informacji uzyskanych z obiektu klasy JModel, ktra w procesie dekodowania
+ * (metoda decode) wytworzy obiekt klasy String o strukturze jak w
+ * przyk�adzie.
  * 
- * @author Michał Krakiewicz
- *
+ * String do przetworzenia w JModel :
+ * java.lang.Osoba{imie(java.lang.String):x;nazwisko(java.lang.String):y;}
+ * 
+ * @author Micha� Krakiewicz
+ * 
  */
 public class JSerializeReaderImpl implements JSerializeReader {
 
+	/**
+	 * class member holding the deserialized object
+	 */
 	private Object deserializedObject;
-	
+
+	/**
+	 * object hash map
+	 * 
+	 */
 	private Map<String, Object> objectHashMap;
 
+	/**
+	 * Creates the object from hashmap using java reflection
+	 * 
+	 *  (non-Javadoc)
+	 * @see exesoft.JSerializeReader#fromMap(java.util.Map)
+	 */
 	@Override
 	public Object fromMap(Map<String, Object> map) {
 		Object ob = new Object();
 		
+		Object inner = map.keySet();
+		Class toDeserialize = Class.forName(map., initialize, loader)
+
 		return deserializedObject;
 
 	}
 
+	/**
+	 * Reads the object from input stream.
+	 * 
+	 * @see exesoft.JSerializeReader#readObject(java.io.InputStream)
+	 */
 	@Override
 	public Boolean readObject(InputStream input) {
 		JModel parser = new JModel();
 
-		/** 
+		/**
 		 * Convert InputStream to string:
 		 */
-		
-		//Create BufferedReader object
-        BufferedReader bReader = new BufferedReader(new InputStreamReader(input));
-        
-        StringBuffer sbf = new StringBuffer();
-        String line = null;
-       
-        //read line by line
-        try {
-			while( (line = bReader.readLine()) != null){
-			        sbf.append(line);
+
+		// Create BufferedReader object
+		BufferedReader bReader = new BufferedReader(
+				new InputStreamReader(input));
+
+		StringBuffer sbf = new StringBuffer();
+		String line = null;
+
+		// read line by line
+		try {
+			while ((line = bReader.readLine()) != null) {
+				sbf.append(line);
 			}
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 			return false;
 		}
-    
-       
-        objectHashMap = parser.decode(sbf.toString());
-        
-        if (objectHashMap == null)
-        	return false;
-		
+
+		objectHashMap = parser.decode(sbf.toString());
+
+		if (objectHashMap == null)
+			return false;
+
 		return true;
 	}
 
-	public Map<String, Object> sampleHashMap() {
+	private Map<String, Object> sampleHashMap() {
 		/**
 		 * 
 		 * { "Osoba" => { [0] => {"String" => "x" }, [1] => {"String" => "y" }
@@ -83,18 +103,12 @@ public class JSerializeReaderImpl implements JSerializeReader {
 		Map<String, Object> y = new HashMap<String, Object>();
 		y.put("String", "y");
 		x.put("String", "x");
-		inner.put("0",x);
-		inner.put("1",y);
+		inner.put("0", x);
+		inner.put("1", y);
 		tmp.put("Osoba", inner);
-		
-		
+
 		return tmp;
-		
+
 	}
-	
-	public static void main(String[] args) {
-		JSerializeReaderImpl r = new JSerializeReaderImpl();
-		Map<String, Object> m = r.sampleHashMap();
-		System.out.println(m.toString());
-	}
+
 }
