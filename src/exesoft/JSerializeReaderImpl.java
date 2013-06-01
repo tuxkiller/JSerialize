6126fef262e310583dfc62f338d694a44cb0d17d
Michał Krakiewicz <michalkrakiewicz@hotmail.com>
2013-06-01 23:34:10 +0200

    dostosowanie klasy do JSerializeWriterImpl.java

--- src/exesoft/JSerializeReaderImpl.java
+++ src/exesoft/JSerializeReaderImpl.java
@@ -4,16 +4,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.security.InvalidParameterException;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
 /**
  * 
  * JSerializeReader - Klasa odpowiedzialna za odwzorowanie obiektu na podstawie
  * informacji uzyskanych z obiektu klasy JModel, ktra w procesie dekodowania
- * (metoda decode) wytworzy obiekt klasy String o strukturze jak w
- * przyk³adzie.
+ * (metoda decode) wytworzy obiekt klasy String o strukturze jak w przyk³adzie.
  * 
  * String do przetworzenia w JModel :
  * java.lang.Osoba{imie(java.lang.String):x;nazwisko(java.lang.String):y;}
@@ -22,6 +24,40 @@ import java.util.Set;
  * 
  */
 public class JSerializeReaderImpl implements JSerializeReader {
+
+	/**
+	 * Helper class for holding fields data
+	 * 
+	 * @author Micha³
+	 * 
+	 */
+	class JSONElement {
+
+		private String name;
+		private String type;
+		private String joinedString;
+		private Object innerTypes;
+
+		public JSONElement(String name, String type, Object innerTypes) {
+
+			this.name = name;
+			this.type = type;
+			this.innerTypes = innerTypes;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public String getType() {
+			return type;
+		}
+
+		public Object getInnerTypes() {
+			return innerTypes;
+		}
+
+	}
 
 	/**
 	 * class member holding the deserialized object
@@ -37,17 +73,36 @@ public class JSerializeReaderImpl implements JSerializeReader {
 	/**
 	 * Creates the object from hashmap using java reflection
 	 * 
-	 *  (non-Javadoc)
+	 * (non-Javadoc)
+	 * 
 	 * @see exesoft.JSerializeReader#fromMap(java.util.Map)
 	 */
 	@Override
 	public Object fromMap(Map<String, Object> map) {
 		Object ob = new Object();
-	
-		Set inner = map.keySet();
-		inner.getClass();
-		inner.
-//		Class toDeserialize = Class.forName(inner., initialize, loader)
+
+		JSONElement entry = decodeHashMapEntry(map);
+		
+		String name = entry.getName();
+		Class toDeserialize = null;
+
+		try {
+			
+			toDeserialize = Class.forName(name, false, null);
+		} catch (ClassNotFoundException e) {
+			// TODO: add msg
+			e.printStackTrace();
+		} catch (NoSuchElementException e) {
+			// TODO: add msg
+			e.printStackTrace();
+		}
+
+		try {
+			ob = toDeserialize.newInstance();
+		} catch (InstantiationException | IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 		return deserializedObject;
 
@@ -60,7 +115,7 @@ public class JSerializeReaderImpl implements JSerializeReader {
 	 */
 	@Override
 	public Boolean readObject(InputStream input) {
-		JModel parser = new JModel();
+		JModel parser = new JModelImpl();
 
 		/**
 		 * Convert InputStream to string:
@@ -92,24 +147,21 @@ public class JSerializeReaderImpl implements JSerializeReader {
 		return true;
 	}
 
-	public static Map<String, Object> sampleHashMap() {
-		/**
-		 * 
-		 * { "Osoba" => { [0] => {"String" => "x" }, [1] => {"String" => "y" }
-		 * 
-		 * }
-		 */
-		Map<String, Object> tmp = new HashMap<String, Object>();
-		Map<String, Object> inner = new HashMap<String, Object>();
-		Map<String, Object> x = new HashMap<String, Object>();
-		Map<String, Object> y = new HashMap<String, Object>();
-		y.put("String", "y");
-		x.put("String", "x");
-		inner.put("0", x);
-		inner.put("1", y);
-		tmp.put("Osoba", inner);
+	public static JSONElement decodeHashMapEntry(Map<String, Object> map) {
 
-		return tmp;
+		Set keys = map.keySet();
+		if (keys.size() == 0) {
+			// empty hashmap?
+			return null;
+		} else if (keys.size() > 1) {
+			// hashmap should contain only 1 entry!
+			throw new InvalidParameterException("Hashmap: \n" + map.toString()
+					+ "\ncontains more than 1 entry!");
+		}
+		Iterator<String> tmp = keys.iterator();
+		String encodedName = tmp.next();
+
+		return null;
 
 	}
 
