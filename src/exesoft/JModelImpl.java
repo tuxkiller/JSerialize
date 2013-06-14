1890cbeb95c6c7495c03a394862957eec8dd5d9e
Wojciech Mucha <mortus1991@gmail.com>
2013-06-14 06:16:42 +0200

    Prawie jak decoder

--- src/exesoft/JModelImpl.java
+++ src/exesoft/JModelImpl.java
@@ -1,10 +1,15 @@
 package exesoft;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.IntArrayData;
+import com.sun.xml.internal.fastinfoset.util.CharArray;
 
 /**
  * 
@@ -36,7 +41,7 @@ public class JModelImpl implements JModel {
 			} else if (pairs.getValue() instanceof Map) {
 				bufferJson.append(getName((String) pairs.getKey()));
 				bufferJson.append(": {");
-				bufferJson.append(handleMap((Map<String, Object>) pairs
+				bufferJson.append(handleMap((Map) pairs
 						.getValue()));
 
 			} else {
@@ -79,7 +84,7 @@ public class JModelImpl implements JModel {
 		} else if (list.get(0) instanceof Map) {
 			bufferJson.append(":[ ");
 			for (int i = 0; i < list.size(); i++) {
-				bufferJson.append(handleMap((Map<String, Object>) list.get(i)));
+				bufferJson.append(handleMap((Map) list.get(i)));
 				if (i < list.size() - 1)
 					bufferJson.append(", ");
 			}
@@ -106,31 +111,31 @@ public class JModelImpl implements JModel {
 		while (it.hasNext()) {
 			Map.Entry pairs = (Map.Entry) it.next();
 			if (pairs.getValue() instanceof List) {
-				bufferJson.append(getName((String) pairs.getKey()));
+				bufferJson.append(getName(pairs.getKey().toString()));
 				bufferJson.append(handleList((List) pairs.getValue()));
 				if (it.hasNext())
 					bufferJson.append(", ");
 
 			} else if (pairs.getValue() instanceof Map) {
-				bufferJson.append(getName((String) pairs.getKey()));
+				bufferJson.append(getName(pairs.getKey().toString()));
 				bufferJson.append(": {");
-				bufferJson.append(handleMap((Map<String, Object>) pairs
+				bufferJson.append(handleMap((Map) pairs
 						.getValue()));
 
 			} else {
 				if (!it.hasNext()) {
 					if (pairs.getKey().toString().contains("value"))
-						bufferJson.append(getName((String) pairs.getKey())
+						bufferJson.append(getName(pairs.getKey().toString())
 								+ ": { " + pairs.getValue() + " ");
 					else
 						bufferJson.append(pairs.getKey() + ":"
 								+ pairs.getValue() + " ");
 				} else {
 					if (pairs.getKey().toString().contains("value"))
-						bufferJson.append(getName((String) pairs.getKey())
+						bufferJson.append(getName(pairs.getKey().toString())
 								+ ": { " + pairs.getValue() + ", ");
 					else
-						bufferJson.append(getName((String) pairs.getKey())
+						bufferJson.append(getName(pairs.getKey().toString())
 								+ ":" + pairs.getValue() + ", ");
 				}
 			}
@@ -141,13 +146,111 @@ public class JModelImpl implements JModel {
 		return bufferJson.toString();
 	}
 
-	@Override
-	public Map<String, Object> decode(String fromJson) {
-		Map<String, Object> outJson = new HashMap<String, Object>();
-
-		return outJson;
+	
+	private void textSearcher (String text){
+		
 	}
 
+	@Override
+	public Map<String, Object> decode(String fromJson) {
+		Map<String, Object> toJSON = new HashMap<String, Object>();
+		String Obiekt;
+		StringBuffer buffer = new StringBuffer(fromJson);
+		StringBuffer klucz = new StringBuffer("");
+		Object wartosc = null;
+		int temp;
+		int nawOtw = 0;
+		int nawZam = 0;
+		String MainClass = buffer.substring(0, buffer.indexOf(" {"));
+		buffer.delete(0, buffer.indexOf("{")+1);
+		//System.out.println("B: " + buffer.toString());
+		nawOtw++;
+		while (nawOtw!=nawZam)
+		{
+			if(buffer.charAt(0)=='}')
+			{
+				nawZam++;
+				buffer.deleteCharAt(0);
+			}
+				
+			//"NAZWA":[V, V]
+			if (buffer.indexOf("\"")==0) 
+			{
+				buffer.deleteCharAt(0);//USUWAMY "
+				temp = buffer.indexOf("\"");
+				klucz.append(buffer.substring(0, temp));
+				buffer.delete(0, temp+1);//USUWAMY CIAG ORAZ "
+				//System.out.println("Klucz: " + klucz + " w buferze: " + buffer);
+				if (buffer.substring(0, 1) == ":[") 
+				{
+					//usuwamy ":"  zostaje [10, 2] + smieci...
+					buffer.deleteCharAt(0);//USUWAMY :
+					System.out.println("Klucz: " + klucz + " w buferze: " + buffer);
+					
+					// CZY INT ARRAY
+					if (isWrapperType(IntArrayData.class))
+					{
+						klucz.append("#IntArray");
+						
+						String toIntArrayBuffer = buffer.substring(0, buffer.indexOf("]")+1);
+						String[] items=toIntArrayBuffer.replaceAll("\\[", "")
+								.replaceAll("\\]","")
+									.split(", ");
+
+						int[] results = new int[items.length];
+						for (int i = 0; i < items.length; i++) {
+						    try {
+						        results[i] = Integer.parseInt(items[i]);
+						    } catch (NumberFormatException nfe) {};
+						}
+						//INT[] TO LIST 
+					    List<Integer> intList = new ArrayList<Integer>();
+					    for (int index = 0; index < results.length; index++)
+					    {
+					        intList.add(results[index]);
+					    }
+					    //KONIEC: INT[] TO LIST 
+					    
+					    toJSON.put(klucz.toString(), intList);
+					} 
+				} else if (buffer.substring(0, 2) == ": {"){
+					nawOtw++;
+					if (isWrapperType(List.class)){
+						klucz.append("#java.util.List");
+						System.out.println("TO LISTA");
+					}
+				} else if (buffer.charAt(0) == ':')
+				{
+				buffer.deleteCharAt(0);//USUWAMY :
+				System.out.println("Klucz: " + klucz + " w buferze: " + buffer);
+				//wartosc = buffer.substring(0, buffer.indexOf(", "));
+				if (isNumeric(buffer.substring(0, buffer.indexOf(", "))))
+				{
+					buffer.delete(0, buffer.indexOf(", "));
+					klucz.append("#Integer");
+					toJSON.put(klucz.toString(), buffer.substring(0, buffer.indexOf(", ")));
+				}
+				}
+			}
+
+			}
+		return toJSON;
+		}
+		
+		
+		
+		
+	
+
+	public static boolean isNumeric(String str)
+	{
+	    for (char c : str.toCharArray())
+	    {
+	        if (!Character.isDigit(c)) return false;
+	    }
+	    return true;
+	}
+	
 	private String getName(String str) {
 		String out = new String();
 
@@ -163,4 +266,30 @@ public class JModelImpl implements JModel {
 
 		return "\"" + out + "\"";
 	}
+	private static final HashSet<Class<?>> WRAPPER_TYPES = getWrapperTypes();
+	
+	
+	private static HashSet<Class<?>> getWrapperTypes()
+    {
+        HashSet<Class<?>> ret = new HashSet<Class<?>>();
+        ret.add(Boolean.class);
+        ret.add(Character.class);
+        ret.add(Byte.class);
+        ret.add(Short.class);
+        ret.add(Integer.class);
+        ret.add(Long.class);
+        ret.add(Float.class);
+        ret.add(Double.class);
+        ret.add(Void.class);
+        ret.add(IntArrayData.class);
+        ret.add(CharArray.class);
+        ret.add(String.class);
+        ret.add(List.class);
+        return ret;
+    }
+	
+	public static boolean isWrapperType(Class<?> clazz)
+    {
+        return WRAPPER_TYPES.contains(clazz);
+    }
 }
